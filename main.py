deeb05100cb32e42ac90b5b3dd1f1f6286784479
Raphael B. Dias <86736451+raphaelbdias@users.noreply.github.com>
2024-01-31 18:19:41 +0000

    DB updated into the repo

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 from flask import Flask, render_template
+from models import Personnel, Equipment, Vehicles, Materials, MineSections, db
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///instance/site.db'
+db.init_app(app)
 
 @app.route('/')
 def home():
-    # Display key metrics on the home/summary page
-    return render_template('home.html', metrics={})
+    # Fetch some data from models
+    personnel_data = Personnel.query.all()
+    equipment_data = Equipment.query.all()
+    vehicles_data = Vehicles.query.all()
+    materials_data = Materials.query.all()
+    mine_sections_data = MineSections.query.all()
 
-# run the file
+    # Pass the data to the template
+    return render_template('home.html',
+                           personnel_data=personnel_data,
+                           equipment_data=equipment_data,
+                           vehicles_data=vehicles_data,
+                           materials_data=materials_data,
+                           mine_sections_data=mine_sections_data)
+
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
