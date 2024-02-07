434959af560c159b16b0a15fdff1b24d6b37c3f3
Raphael B. Dias <86736451+raphaelbdias@users.noreply.github.com>
2024-02-07 02:09:18 +0000

    Test success on vehcile and personnel table join

--- testapp.py
+++ testapp.py
@@ -17,8 +17,8 @@ with app.app_context():
     mine_sections_data = MineSections.query.all()
     
     
-    for person in personnel_data:
-        print(f"Person: {person.name}, Equipment: {person.equipment.equipment_id}, Vehicle: {person.vehicle.fuel_consumption}")
+    # for person in personnel_data:
+    #     print(f"Person: {person.name}, Equipment: {person.equipment.equipment_id}, Vehicle: {person.vehicle.fuel_consumption}")
         
 
     # for equipment in equipment_data:
@@ -29,9 +29,9 @@ with app.app_context():
             
             
     data = []
-    for equipment in Equipment.query.all():
-        dictionary = {"Equipment": equipment.equipment_id, "Status": equipment.status, "personnel":"", "system": equipment.type_of_systems, "usage_statistics":equipment.usage_statistics}
-        for i in equipment.personnel:
+    for vehicles in Vehicles.query.all():
+        dictionary = {"vehicles": vehicles.vehicle_type, "Fuel": vehicles.fuel_consumption, "material":"", "maintenance_history": vehicles.maintenance_history, "distance_traveled":vehicles.distance_traveled}
+        for i in vehicles.personnel:
             dictionary.update({"personnel":i.name})  
         data.append(dictionary)
     print(data)
