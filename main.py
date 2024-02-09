9c30dce55fdd5c5bcd93187abba1868e26f755ae
Raphael B. Dias <86736451+raphaelbdias@users.noreply.github.com>
2024-02-09 04:33:59 +0000

    updates

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import plotly
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
 db.init_app(app)
 
 with app.app_context():
@@ -16,37 +16,35 @@ with app.app_context():
     materials_data = Materials.query.all()
     mine_sections_data = MineSections.query.all()
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    
     # Pass the data to the template
-    return render_template('index.html',
-                            personnel_data=personnel_data,
-                           equipment_data=equipment_data,
-                           vehicles_data=vehicles_data,
-                           materials_data=materials_data,
-                           mine_sections_data=mine_sections_data)
+    return render_template(
+        "index.html",
+        personnel_data=personnel_data,
+        equipment_data=equipment_data,
+        vehicles_data=vehicles_data,
+        materials_data=materials_data,
+        mine_sections_data=mine_sections_data,
+    )
 
-@app.route('/personnel')
+
+@app.route("/personnel")
 def personnel():
-    
-
     # Pass the data to the template
-    return render_template('personnel.html',
-                           personnel_data=personnel_data)
+    return render_template("personnel.html", personnel_data=personnel_data)
 
-@app.route('/vehicles')
+
+@app.route("/vehicles")
 def vehicles():
-    
     # Pass the data to the template
-    return render_template('vehicles.html',
-                            vehicles_data=vehicles_data)
+    return render_template("vehicles.html", vehicles_data=vehicles_data)
 
 
-
-@app.route('/equipment')
+@app.route("/equipment")
 def equipment():
-    graphJSON = ''
+    graphJSON = ""
 
     data = []
     for equipment in Equipment.query.all():
@@ -54,72 +52,63 @@ def equipment():
         dictionary = {
             "Equipment": equipment.equipment_id,
             "Status": equipment.status,
-            "personnel": ', '.join(personnel_names),
+            "personnel": ", ".join(personnel_names),
             "system": equipment.type_of_systems,
-            "usage_statistics": equipment.usage_statistics
+            "usage_statistics": equipment.usage_statistics,
         }
         data.append(dictionary)
 
     # Create a bar chart
-    fig = go.Figure(data=[
-        go.Bar(x=[entry['Equipment'] for entry in data],
-               y=[entry['usage_statistics'] for entry in data],
-               text=[entry['system'] for entry in data],
-               marker=dict(color='#305D7D'),
-               
-        )
-    ])
+    fig = go.Figure(
+        data=[
+            go.Bar(
+                x=[entry["Equipment"] for entry in data],
+                y=[entry["usage_statistics"] for entry in data],
+                text=[entry["system"] for entry in data],
+                marker=dict(color="#305D7D"),
+            )
+        ]
+    )
 
     # Update layout for better visualization
- 
+
     fig.update_layout(
         font_family="Source Sans Pro",
         font_color="#000",
-        title='Equipment Usage Statistics',
-        xaxis_title='Equipment',
-        yaxis_title='Usage Statistics',
-        barmode='group',
+        title="Equipment Usage Statistics",
+        xaxis_title="Equipment",
+        yaxis_title="Usage Statistics",
+        barmode="group",
         width=500,
         height=400,
         dragmode=False,
         hoverlabel_font_family="Source Sans Pro",
         paper_bgcolor="#fff",
-        plot_bgcolor="#fff"
-
+        plot_bgcolor="#fff",
     )
-    fig.update_xaxes(showline=True,
-                    color="#000",
-                    linewidth=1,
-                    linecolor='black',
-                    mirror=True)
+    fig.update_xaxes(
+        showline=True, color="#000", linewidth=1, linecolor="black", mirror=True
+    )
 
-    fig.update_yaxes(showline=True,
-                    linewidth=1,
-                    linecolor='black',
-                    mirror=True)
-
-
-
+    fig.update_yaxes(showline=True, linewidth=1, linecolor="black", mirror=True)
 
     # Convert the figure to JSON for rendering in HTML
     graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
 
-    return render_template('equipment.html', graphJSON=graphJSON, data = data)
+    return render_template("equipment.html", graphJSON=graphJSON, data=data)
 
-@app.route('/mine')
+
+@app.route("/mine")
 def mine():
-    
     # Pass the data to the template
-    return render_template('mine.html',
-                            mine_sections_data=mine_sections_data)
+    return render_template("mine.html", mine_sections_data=mine_sections_data)
 
 
-@app.route('/materials')
+@app.route("/materials")
 def materials():
-    
     # Pass the data to the template
-    return render_template('materials.html',
-                            materials_data=materials_data)
+    return render_template("materials.html", materials_data=materials_data)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True, port=8080)
